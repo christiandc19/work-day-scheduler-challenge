5ab028baf28ac4f04065ec018d40baf66bac1a66
unknown <christiandc19@hotmail.com>
2022-01-29 19:40:02 -0800

    updated readme file - added a screenshot - changed button icon size

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -3,15 +3,14 @@ var californiaTime = document.querySelector("#currentDay");
 
 
 var currentTime = moment();
-californiaTime.textContent = currentTime.format('MMMM Do YYYY, h:mm:ss a'); // January 28th 2022, 11:52:38 pm
+californiaTime.textContent = currentTime.format('MMMM Do YYYY, h:mm a'); // January 28th 2022, 11:52:38 pm
 var currentHour = moment().format('H'); // January 28th 2022, 11:52:38 pm
 var am = true;
 
 
 // create elements for timeblock
-function showSched() {
-    // $(".container").empty();
-    var officeHours = 10;
+    function showSched() {
+    var officeHours = 9;
 
     for (var i = 1; i < 10; i++) {
       var containerRow = $("<div class='row'>");
@@ -33,7 +32,7 @@ function showSched() {
         taskBox.addClass("past");
       }
 
-      var btnBox = $("<button class='col-1 time-block saveBtn' id='btn-box'>Save</button>" );
+      var btnBox = $("<button class='col-1 time-block saveBtn far fa-save fa-2x' id='btn-box'></button>" );
 
       // Append Container Elements
       $(".container").append(containerRow);
@@ -46,16 +45,13 @@ function showSched() {
         officeHours = 0;
         am = false;
         officeHours++;
-console.log(officeHours);
-console.log(currentHour)
       }
     
     // save-item functon in a closure function
 
-    function saveFunc(index) {
+      function saveFunc(index) {
       btnBox.click(function() {
 
-        var taskContainer = $(".description-" + index); //get the value of the textarea
         var task = $(".description-" + index).val(); //get the value of the textarea
 
         if(index === 1){
@@ -121,164 +117,12 @@ function getVal() {
   document.querySelector(".description-8").innerHTML = getTask4pm;
   console.log(getTask4pm);
 
-  var getTask5pm = localStorage.getItem("task8");
-  document.querySelector(".description-8").innerHTML = getTask5pm;
+  var getTask5pm = localStorage.getItem("task9");
+  document.querySelector(".description-9").innerHTML = getTask5pm;
   console.log(getTask5pm);
 
 
 }
 getVal();
-
-
-
-// // SAVE TO LOCAL STORAGE FUNCTION
-// function saveItems() {
-//     var Task9am = document.getElementById("textarea9").value;
-//     localStorage.setItem("task", Task9am);
-
-//     var Task10am = document.getElementById("textarea10").value;
-//     localStorage.setItem("task10", Task10am);
-
-//     var Task11am = document.getElementById("textarea11").value;
-//     localStorage.setItem("task11", Task11am)
-
-//     var Task12nn = document.getElementById("textarea12").value;
-//     localStorage.setItem("task12", Task12nn)
-
-//     var Task1pm = document.getElementById("textarea1").value;
-//     localStorage.setItem("task1", Task1pm)
-
-//     var Task2pm = document.getElementById("textarea2").value;
-//     localStorage.setItem("task2", Task2pm)
-
-//     var Task3pm = document.getElementById("textarea3").value;
-//     localStorage.setItem("task3", Task3pm)
-
-//     var Task4pm = document.getElementById("textarea4").value;
-//     localStorage.setItem("task4", Task4pm)
-    
-//     var Task5pm = document.getElementById("textarea5").value;
-//     localStorage.setItem("task5", Task5pm)
-//   }
-
-// // RETRIEVE FROM LOCAL STORAGE FUNCTION
-//   getSavedItems = () => {
-//     var gettask9 = localStorage.getItem("task");
-//     document.getElementById("textarea9").innerHTML = gettask9;
-
-//     var gettask10 = localStorage.getItem("task10");
-//     document.getElementById("textarea10").innerHTML = gettask10;
-
-//     var gettask11 = localStorage.getItem("task11");
-//     document.getElementById("textarea11").innerHTML = gettask11;
- 
-//     var gettask12 = localStorage.getItem("task12");
-//     document.getElementById("textarea12").innerHTML = gettask12;
-
-//     var gettask1 = localStorage.getItem("task1");
-//     document.getElementById("textarea1").innerHTML = gettask1;
-
-//     var gettask2 = localStorage.getItem("task2");
-//     document.getElementById("textarea2").innerHTML = gettask2;
-
-//     var gettask3 = localStorage.getItem("task3");
-//     document.getElementById("textarea3").innerHTML = gettask3;
-
-//     var gettask4 = localStorage.getItem("task4");
-//     document.getElementById("textarea4").innerHTML = gettask4;
-
-//     var gettask5 = localStorage.getItem("task5");
-//     document.getElementById("textarea5").innerHTML = gettask5;
-// }
-//   getSavedItems();
-
-
-//   save9.addEventListener("click", saveItems)
-//   save10.addEventListener("click", saveItems)
-//   save11.addEventListener("click", saveItems)
-//   save12.addEventListener("click", saveItems)
-//   save1.addEventListener("click", saveItems)
-//   save2.addEventListener("click", saveItems)
-//   save3.addEventListener("click", saveItems)
-//   save4.addEventListener("click", saveItems)
-//   save5.addEventListener("click", saveItems)
-
-
-//var save9 = document.querySelector("#save9am") // 9:00 AM Save Button
-// var save10 = document.querySelector("#save10am") // 10:00 AM Save Button
-// var save11 = document.querySelector("#save11am") // 11:00 AM Save Button
-// var save12 = document.querySelector("#save12nn") // 12:00 NN Save Button
-// var save1 = document.querySelector("#save1pm") // 1:00 PM Save Button
-// var save2 = document.querySelector("#save2pm") // 2:00 PM Save Button
-// var save3 = document.querySelector("#save3pm") // 3:00 PM Save Button
-// var save4 = document.querySelector("#save4pm") // 4:00 PM Save Button
-// var save5 = document.querySelector("#save5pm") // 5:00 PM Save Button
-
-
-
-
-
-
-
-
-//   9:00 AM
-//   <div class="row">
-//     <p class="timeBlockItem col-1 border border-info text-center" id="text-9am">9:00AM</p>
-//     <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea9" cols="30" rows="10"></textarea>
-//     <button class="timeBlockItem saveBtn col-1 border border-info" id="save9am">Save</button>
-//   </div>
-// <!-- 10:00 AM -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="text-10am">10:00AM</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea10" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save10am">Save</button>
-// </div>
-// <!-- 11:00 AM -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="task1am">11:00AM</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea11" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save11am">Save</button>
-// </div>
-// <!-- 12:00 NN -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="task2nn">12:00NN</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea12" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save12nn">Save</button>
-// </div>
-
-// <!-- 1:00 PM -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="taskpm">1:00PM</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea1" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save1pm">Save</button>
-// </div>
-
-// <!-- 2:00 PM -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="text2pm">2:00PM</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea2" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save2pm">Save</button>
-// </div>
-
-// <!-- 3:00 PM -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="text3pm">3:00PM</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea3" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save3pm">Save</button>
-// </div>
-
-// <!-- 4:00 PM -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="text4pm">4:00PM</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea4" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save4pm">Save</button>
-// </div>
-
-// <!-- 5:00 PM -->
-// <div class="row ">
-//   <p class="timeBlockItem col-1 border border-info text-center" id="text5pm">5:00PM</p>
-//   <textarea class="timeBlockItem col-10 border border-info textarea1" name="" id="textarea5" cols="30" rows="10"></textarea>
-//   <button class="timeBlockItem saveBtn col-1 border border-info" id="save5pm">Save</button>
-// </div>
 
 
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler 
+A calendar app that helps yo organize your day
+
+## Challenge Goal
+
+Create a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+## Technologies
+
+- Html
+- Javascript
+- CSS
+- Bootstrap
+- JQuery
+- Moment.js
+
+## Challeng Screenshot
+
+![screenshot](./Develop/assets/images/proj-screenshot.png)
+
+
+## Deployment
+
+https://christiandc19.github.io/work-day-scheduler-challenge/
+
+https://github.com/christiandc19/work-day-scheduler-challenge
